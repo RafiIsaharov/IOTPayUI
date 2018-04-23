72f5e6cac64d075d8b2938cdaf1399334d7cafb8
Tomer Abramovich <TomerA@fundtech.isr>
2018-04-23 13:10:40 +0300

    small checkin for rollback test

--- src/services/mock-devices.ts
+++ src/services/mock-devices.ts
@@ -1,9 +1,12 @@
 export let ITEMS = [
   {
+    
+    id: '123',
     name: 'Coffe Machine',
     desc:'provide beans',
     img:'../assets/img/iot/coffee-machine.png'},
   {
+    id: '456',
     name: 'Dush button',
     desc:'order bear',
     img:'../assets/img/iot/iotbutton.png'}
