a3b5e89f40f07ac0416645b62ff312ecfc8f85c6
Rafi Isaharov <RafiI@fundtech.isr>
2018-04-26 21:37:31 +0300

    added Alex to  mock

--- src/services/mock-user-devices.ts
+++ src/services/mock-user-devices.ts
@@ -3,16 +3,16 @@ export let USER_DEVICES = [
        
       userName: 'Tomer',
       deviceId: [123,456] 
-    },
-    
+    },    
     {
         userName: 'Rafi',
         deviceId: [789] 
       }, {
         userName: 'John',
         deviceId: [456] 
-      }
-
-
-      
+      },
+      { 
+        userName: 'Alex',
+        deviceId: [789] 
+      },
   ]
--- src/services/mock-users.ts
+++ src/services/mock-users.ts
@@ -12,12 +12,20 @@ export let USERS = [
       loanOffer:true,
       phone:'+97254202222',
       email:'Rafi.Isaharov@finastra.com',
-      finastraId:'6574-2311'},
+      finastraId:'6274-2311'},
       { 
         userName: 'John',
         accNumber: '24394251235',
         loanOffer:true,
         phone:'+972523669002',
-        email:'JohnLewis@gmail.com',
+        email:'JohnSmith@gmail.com',
         finastraId:'9174-3411'},
+        { 
+          userName: 'Alex',
+          accNumber: '243223523532535',
+          loanOffer:true,
+          phone:'+97254202222',
+          email:'AlexPerman@finastra.com',
+          finastraId:'6574-2311'},
+
   ]
